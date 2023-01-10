13fb8a93306dcc3a274771578813e803257c2665
johyeonuk <dkdldidldi@gmail.com>
2023-01-10 11:38:45 +0900

    Room_ImgDTO 추가

---
+++ citrushotel/src/main/java/com/citrus/hotel/dto/Room_ImgDTO.java
@@ -0,0 +1,14 @@
+package com.citrus.hotel.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Room_ImgDTO {
+	private int img_no;
+	private String img_tit;
+	private String room_no;
+	private String img_path;
+	private String img_nm;
+}
