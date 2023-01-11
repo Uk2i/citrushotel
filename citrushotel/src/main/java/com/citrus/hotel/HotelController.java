46262a7e1d4ed110ea85e43c798d39d81b36f73c
johyeonuk <dkdldidldi@gmail.com>
2023-01-11 17:26:06 +0900

    hoit

--- citrushotel/src/main/java/com/citrus/hotel/HotelController.java
+++ citrushotel/src/main/java/com/citrus/hotel/HotelController.java
@@ -1,16 +1,25 @@
 package com.citrus.hotel;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.citrus.hotel.dto.RoomDTO;
+import com.citrus.hotel.service.HotelMapper;
 
 
 @Controller
 public class HotelController {	
+	@Autowired
+	private HotelMapper hotelMapper;
+	
 	
 	@RequestMapping(value = {"/","index.do"}, method = RequestMethod.GET)
 	public String home(HttpServletRequest req) {
@@ -19,7 +28,10 @@ public class HotelController {
 	}
 	
 	@RequestMapping("rooms.do")
-	public String rooms(HttpServletRequest req) {
+	public String rooms(HttpServletRequest req, @RequestParam Map<String,Object> map) {
+		List<RoomDTO> list = hotelMapper.listRoom(map);
+		req.setAttribute("roomList", list);
+		
 		return "hotel/rooms";
 	}
 	
--- citrushotel/src/main/java/com/citrus/hotel/dto/DTO.java
+++
@@ -1,5 +0,0 @@
-package com.citrus.hotel.dto;
-
-public class DTO {
-	private int Member_no;
-}
